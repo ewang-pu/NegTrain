52dac0bc372e056174bdd52f36dcd83b42d9b3e1
Evan Wang <evanwang@princeton.edu>
2024-01-02 19:03:17 -0500

    requests

--- src/open_clip/pretrained.py
+++ src/open_clip/pretrained.py
@@ -2,29 +2,30 @@ import hashlib
 import os
 import urllib
 import warnings
+import requests
 
 from tqdm import tqdm
 
 _RN50 = dict(
     openai="https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
     yfcc15m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn50-quickgelu-yfcc15m-455df137.pt",
-    cc12m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn50-quickgelu-cc12m-f000538c.pt"
+    cc12m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn50-quickgelu-cc12m-f000538c.pt",
 )
 
 _RN50_quickgelu = dict(
     openai="https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt",
     yfcc15m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn50-quickgelu-yfcc15m-455df137.pt",
-    cc12m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn50-quickgelu-cc12m-f000538c.pt"
+    cc12m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn50-quickgelu-cc12m-f000538c.pt",
 )
 
 _RN101 = dict(
     openai="https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
-    yfcc15m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn101-quickgelu-yfcc15m-3e04b30e.pt"
+    yfcc15m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn101-quickgelu-yfcc15m-3e04b30e.pt",
 )
 
 _RN101_quickgelu = dict(
     openai="https://openaipublic.azureedge.net/clip/models/8fa8567bab74a42d41c5915025a8e4538c3bdbe8804a470a72f30b0d94fab599/RN101.pt",
-    yfcc15m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn101-quickgelu-yfcc15m-3e04b30e.pt"
+    yfcc15m="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/rn101-quickgelu-yfcc15m-3e04b30e.pt",
 )
 
 _RN50x4 = dict(
@@ -65,8 +66,8 @@ _VITB16_PLUS_240 = dict(
 
 _VITL14 = dict(
     openai="https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt",
-    laion400m_e31='https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_l_14-laion400m_e31-69988bb6.pt',
-    laion400m_e32='https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_l_14-laion400m_e32-3d133497.pt',
+    laion400m_e31="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_l_14-laion400m_e31-69988bb6.pt",
+    laion400m_e32="https://github.com/mlfoundations/open_clip/releases/download/v0.2-weights/vit_l_14-laion400m_e32-3d133497.pt",
 )
 
 _VITL14_336 = dict(
@@ -91,14 +92,18 @@ _PRETRAINED = {
 
 
 def list_pretrained(as_str: bool = False):
-    """ returns list of pretrained models
+    """returns list of pretrained models
     Returns a tuple (model_name, pretrain_tag) by default or 'name:tag' if as_str == True
     """
-    return [':'.join([k, t]) if as_str else (k, t) for k in _PRETRAINED.keys() for t in _PRETRAINED[k].keys()]
+    return [
+        ":".join([k, t]) if as_str else (k, t)
+        for k in _PRETRAINED.keys()
+        for t in _PRETRAINED[k].keys()
+    ]
 
 
 def list_pretrained_tag_models(tag: str):
-    """ return all models having the specified pretrain tag """
+    """return all models having the specified pretrain tag"""
     models = []
     for k in _PRETRAINED.keys():
         if tag in _PRETRAINED[k]:
@@ -107,7 +112,7 @@ def list_pretrained_tag_models(tag: str):
 
 
 def list_pretrained_model_tags(model: str):
-    """ return all pretrain tags for the specified model architecture """
+    """return all pretrain tags for the specified model architecture"""
     tags = []
     if model in _PRETRAINED:
         tags.extend(_PRETRAINED[model].keys())
@@ -116,11 +121,11 @@ def list_pretrained_model_tags(model: str):
 
 def get_pretrained_url(model: str, tag: str):
     if model not in _PRETRAINED:
-        return ''
+        return ""
     model_pretrained = _PRETRAINED[model]
     tag = tag.lower()
     if tag not in model_pretrained:
-        return ''
+        return ""
     return model_pretrained[tag]
 
 
@@ -128,10 +133,10 @@ def download_pretrained(url: str, root: str = os.path.expanduser("~/.cache/clip"
     os.makedirs(root, exist_ok=True)
     filename = os.path.basename(url)
 
-    if 'openaipublic' in url:
+    if "openaipublic" in url:
         expected_sha256 = url.split("/")[-2]
     else:
-        expected_sha256 = ''
+        expected_sha256 = ""
 
     download_target = os.path.join(root, filename)
 
@@ -140,24 +145,52 @@ def download_pretrained(url: str, root: str = os.path.expanduser("~/.cache/clip"
 
     if os.path.isfile(download_target):
         if expected_sha256:
-            if hashlib.sha256(open(download_target, "rb").read()).hexdigest() == expected_sha256:
+            if (
+                hashlib.sha256(open(download_target, "rb").read()).hexdigest()
+                == expected_sha256
+            ):
                 return download_target
             else:
-                warnings.warn(f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file")
+                warnings.warn(
+                    f"{download_target} exists, but the SHA256 checksum does not match; re-downloading the file"
+                )
         else:
             return download_target
 
-    with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
-        with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True) as loop:
-            while True:
-                buffer = source.read(8192)
-                if not buffer:
-                    break
+    # old method
+    # with urllib.request.urlopen(url) as source, open(download_target, "wb") as output:
+    #     with tqdm(total=int(source.info().get("Content-Length")), ncols=80, unit='iB', unit_scale=True) as loop:
+    #         while True:
+    #             buffer = source.read(8192)
+    #             if not buffer:
+    #                 break
 
-                output.write(buffer)
-                loop.update(len(buffer))
+    #             output.write(buffer)
+    #             loop.update(len(buffer))
 
-    if expected_sha256 and hashlib.sha256(open(download_target, "rb").read()).hexdigest() != expected_sha256:
-        raise RuntimeError(f"Model has been downloaded but the SHA256 checksum does not not match")
+    # requests method
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+
+        # Get total length if available
+        total_length = int(r.headers.get("content-length", 0))
+
+        with open(download_target, "wb") as f, tqdm(
+            total=total_length, unit="iB", unit_scale=True, ncols=80
+        ) as loop:
+            for chunk in r.iter_content(chunk_size=8192):
+                # filter out keep-alive new chunks
+                if chunk:
+                    f.write(chunk)
+                    loop.update(len(chunk))
+
+    if (
+        expected_sha256
+        and hashlib.sha256(open(download_target, "rb").read()).hexdigest()
+        != expected_sha256
+    ):
+        raise RuntimeError(
+            f"Model has been downloaded but the SHA256 checksum does not not match"
+        )
 
     return download_target
